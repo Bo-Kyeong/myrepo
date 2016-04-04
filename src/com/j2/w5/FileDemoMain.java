1ad5b06ed33b8d90ba3761ce07d8741374ddf49b
Bo-Kyeong <gina9698@naver.com>
2016-04-05 01:10:30 +0900

    FileDemoMain

--- src/com/j2/w5/FileDemoMain.java
+++ src/com/j2/w5/FileDemoMain.java
@@ -11,6 +11,9 @@ package com.j2.w5;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.Reader;
 
 public class FileDemoMain{
   public static void main(String[] args) throws Exception{
@@ -18,6 +21,10 @@ public class FileDemoMain{
     char c;
     File readme=null;
     InputStream isReadme = null;
+    InputStreamReader isrReadme = null;
+    BufferedReader brReadme=null;
+    String str;
+    StringBuffer sbuffer = new StringBuffer();
     try{
      File myhome = new File(System.getProperty("user.home"));
      System.out.println("my home is "+myhome);
@@ -27,6 +34,9 @@ public class FileDemoMain{
      
      readme = new File(cwd,"HelloWorld.java");
      System.out.println("readme is "+readme);
+     
+     isReadme=new FileInputStream(readme);
+     System.out.println("Characters printed:");
      
      isReadme = new FileInputStream(readme);
      while((i=isReadme.read()) !=1){
@@ -39,5 +49,24 @@ public class FileDemoMain{
       if(isReadme != null)
         isReadme.close();
     }
+    try{
+            isReadme=new FileInputStream(readme);
+            isrReadme=new InputStreamReader(isReadme,"UTF8");
+            brReadme= new BufferedReader(isrReadme);
+            /*while((i=brReadme.read())!=-1) {
+                c=(char)i;
+                //System.out.println(c);
+                sbuffer.append(c);
+            }
+            System.out.println("--"+sbuffer.toString());*/
+            while((str=brReadme.readLine())!=null) {
+                System.out.println("--"+str);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            if(brReadme != null)
+                brReadme.close();
+        }
   }
 }
