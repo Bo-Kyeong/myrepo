de614ea4a05ad70e7f165ed4c668bd4ed35ac342
Bo-Kyeong <gina9698@naver.com>
2016-05-30 20:37:04 +0900

    hw

---
+++ src/com/j2/hw/CalculatorDecoPlus.java
@@ -0,0 +1,11 @@
+package com.j2.hw;
+
+class CalculatorDecoPlus extends Calculator {
+    public void sum(){
+        System.out.println("+ sum :"+(this.left+this.right));
+    }
+    public void avg(){
+        System.out.println("+ avg :"+(this.left+this.right)/2);
+    }
+} 
+
